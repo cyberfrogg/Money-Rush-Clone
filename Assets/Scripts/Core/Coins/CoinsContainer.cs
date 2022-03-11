e0df554fa719b37d32cca54990e68e228983b38c
cyberfrogg <68637510+cyberfrogg@users.noreply.github.com>
2022-03-11 04:01:49 +0300

    Sorting coin cells by rows

--- Assets/Scripts/Core/Coins/CoinsContainer.cs
+++ Assets/Scripts/Core/Coins/CoinsContainer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -7,24 +8,51 @@ namespace Core.Coins
     {
         [SerializeField] private Cell[] _cells;
 
+        private IReadOnlyCollection<Row> _rows;
         private Transform _origin;
 
         public void Install(Transform origin)
         {
             _origin = origin;
+            _rows = getRows();
         }
         public void AddCoin(Coin coin)
         {
             Cell nearestCell = getFirstFreeCell();
             nearestCell.Attach(coin);
         }
-
         private Cell getFirstFreeCell()
         {
             return _cells
                 .Where(x => x.IsBusy == false)
-                .OrderBy(x => Vector3.Distance(_origin.position, x.transform.position))
+                .OrderBy(x => Vector3.Distance(_origin.position, x.transform.localPosition))
                 .FirstOrDefault();
+        }
+        private IReadOnlyCollection<Row> getRows()
+        {
+            IEnumerable<Cell> cellRows = _cells.OrderBy(x => x.transform.localPosition.x);
+            IEnumerable<float> xPositions = cellRows.Select(x => x.transform.localPosition.x).Distinct();
+
+            List<Row> rows = new List<Row>();
+            foreach (float pos in xPositions)
+            {
+                IEnumerable<Cell> rowCells = cellRows.Where(x => x.transform.localPosition.x == pos);
+                rows.Add(new Row(rowCells, pos));
+            }
+
+            return rows.AsReadOnly();
+        }
+    }
+
+    public class Row
+    {
+        private IEnumerable<Cell> _cells;
+        private float _xPosition;
+
+        public Row(IEnumerable<Cell> cells, float xPosition)
+        {
+            _cells = cells;
+            _xPosition = xPosition;
         }
     }
 }
