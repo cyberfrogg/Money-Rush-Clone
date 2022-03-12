22246dcc9da6a13dbdd0fddf785ba298ec2a96c4
cyberfrogg <68637510+cyberfrogg@users.noreply.github.com>
2022-03-13 00:41:16 +0300

    fbsdk and coin animation

--- Assets/Scripts/Core/Level/LevelSwitch.cs
+++ Assets/Scripts/Core/Level/LevelSwitch.cs
@@ -6,16 +6,37 @@ namespace Core.Level
     public class LevelSwitch
     {
         private LevelSwitchConfig _config;
-        private int _currentLevel = -1;
+        private int _currentLevel
+        {
+            get
+            {
+                return PlayerPrefs.GetInt("level");
+            }
+            set
+            {
+                PlayerPrefs.SetInt("level", value);
+            }
+        }
 
         public LevelSwitch(LevelSwitchConfig config)
         {
             _config = config;
+
+            if (!PlayerPrefs.HasKey("level"))
+            {
+                PlayerPrefs.SetInt("level", 0);
+            }
         }
 
         public void LoadNextLevel()
         {
             _currentLevel++;
+
+            if (_currentLevel >= _config.Levels.Count)
+            {
+                _currentLevel = 0;
+            }
+
             SceneManager.LoadScene(_config.GetLevel(_currentLevel));
         }
         public void RestartLevel()
--- Assets/Scripts/Core/Level/LevelSwitchConfig.cs
+++ Assets/Scripts/Core/Level/LevelSwitchConfig.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Core.Level
@@ -6,6 +8,8 @@ namespace Core.Level
     [CreateAssetMenu(menuName = "Money Rush/Core/Level Switch Config")]
     public class LevelSwitchConfig : ScriptableObject
     {
+        public IReadOnlyCollection<string> Levels { get => _levels.ToList().AsReadOnly(); }
+
         [SerializeField] private string[] _levels = new string[0];
 
         public string GetLevel(int index)
